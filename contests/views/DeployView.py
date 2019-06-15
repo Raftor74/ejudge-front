61e4c5ef6eb540fe80433e741e948d4cfdafd776
Raftor74 <raftor74@mail.ru>
2019-06-15 19:04:17 +0500

    Ejudge template fixturies

--- contests/views/DeployView.py
+++ contests/views/DeployView.py
@@ -10,7 +10,6 @@ class DeployView(View):
 
     def get(self, request, contest_id):
         contest = get_object_or_404(Contests, pk=contest_id)
-        ContestDeploy(contest).deploy()
         context = {'contest': contest}
         return render(request, template_name='contests/deploy.html', context=context)
 
