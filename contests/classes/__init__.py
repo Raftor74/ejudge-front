184c374a9ee22445062b2c21f60a7e4b76d202d5
Raftor74 <raftor74@mail.ru>
2019-06-15 17:10:01 +0500

    Register on contest backend done

--- contests/classes/__init__.py
+++ contests/classes/__init__.py
@@ -1,2 +1,3 @@
 from .ContestManager import ContestCreate, ContestUpdate
 from .ContestDeploy import ContestDeploy
+from .ContestRegisterManager import ContestRegisterManager
--- contests/views/RegisterView.py
+++ contests/views/RegisterView.py
@@ -3,6 +3,7 @@ from django.views import View
 from django.http import JsonResponse
 from tools import admin_denied, Response
 from contests.models import Contests
+from contests.classes import ContestRegisterManager
 
 
 @admin_denied()
@@ -25,6 +26,25 @@ class RegisterView(View):
         return render(request, template_name='contests/register.html', context=context)
 
     def post(self, request, contest_id):
+        user = request.user
+
+        try:
+            contest = Contests.objects.exclude(users__contests__users__exact=user).get(pk=contest_id, is_visible=True)
+        except Contests.DoesNotExist:
+            response = Response(Response.STATUS_FAIL, "Соревнование недоступно для регистрации", {}).get()
+            return JsonResponse(response)
+
+        if not contest.is_deployed:
+            response = Response(Response.STATUS_FAIL, "Соревнование недоступно для регистрации", {}).get()
+            return JsonResponse(response)
+
+        provider = ContestRegisterManager(user=user, contest=contest, post_data=request.POST)
+        success = provider.register_user_on_contest()
+
+        if not success:
+            response = Response(Response.STATUS_FAIL, provider.last_error, {}).get()
+            return JsonResponse(response)
+
         data = {'redirect': reverse('profile')}
         response = Response(Response.STATUS_OK, "", data).get()
         return JsonResponse(response)
